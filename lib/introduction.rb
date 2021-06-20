d04bc4a34290636e089732ef376d663dbd428e60
scailles <scailles23@gmail.com>
2021-06-20 01:36:58 -0700

    commit tree d91f6eb9d03bdc19daf2c7a014736ad12caba4d7

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,6 +7,5 @@ end
 
 
 def introduction_with_language(name,language)
-    puts "Hi, my name is #{name} and I am learning to 
-    to program in #{language}."
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
